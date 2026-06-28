a596d2d9c45bdb1f15f58041b1684da49007fd18
git-ADG <adg8503@gmail.com>
2026-06-28 21:04:03 +0530

    export notes (download) feature

--- src/main/java/com/example/dev_diaries/controllers/NotesController.java
+++ src/main/java/com/example/dev_diaries/controllers/NotesController.java
@@ -6,14 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dev_diaries.models.Format;
 import com.example.dev_diaries.models.Note;
+import com.example.dev_diaries.services.ExportService;
 import com.example.dev_diaries.services.NotesService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,9 +31,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/api/notes")
 public class NotesController {
     NotesService notesService;
+    ExportService exportService;
 
-    public NotesController(NotesService notesService) {
+    public NotesController(NotesService notesService, ExportService exportService) {
         this.notesService = notesService;
+        this.exportService = exportService;
     }
 
     @GetMapping("/")
@@ -52,5 +60,21 @@ public class NotesController {
     @PutMapping("/{id}")
     public Note updateNote(@PathVariable UUID id, @RequestBody Note note){
         return notesService.updateNote(id, note);
+    }
+
+    @GetMapping("/export")
+    public ResponseEntity<byte[]> exportNotes(){
+        try{
+            byte[] zipData = exportService.exportToZip();
+            // System.out.println(zipData.toString());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"dev-diaries-backup.zip\"");
+
+            return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_OCTET_STREAM).body(zipData);
+            
+        }catch(IOException e){
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
